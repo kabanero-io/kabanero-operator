559d6a0bb263548a2ddb4961cf9354bcb23821df
spewak <spewak@us.ibm.com>
2019-07-31 16:00:51 -0400

    update prepare release script to update reository tag

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -2,6 +2,13 @@
 BASEPATH=$(dirname $(dirname $0))
 DEST=$BASEPATH/deploy/kabanero-operators.yaml
 
+# Find we are running on MAC.
+MAC_EXEC=false
+macos=Darwin
+if [ "$(uname)" == "Darwin" ]; then
+   MAC_EXEC=true
+fi
+
 # Prepare operator deployment file
 cat << EOF >> $DEST
 apiVersion: v1
@@ -16,3 +23,12 @@ cat $BASEPATH/deploy/operator.yaml >> $DEST; echo "---" >> $DEST
 cat $BASEPATH/deploy/role.yaml >> $DEST; echo "---" >> $DEST
 cat $BASEPATH/deploy/role_binding.yaml >> $DEST; echo "---" >> $DEST
 cat $BASEPATH/deploy/service_account.yaml >> $DEST; echo "---" >> $DEST
+
+# Update the operator deployment image entry if TRAVIS_TAG is set
+if [ ! -z "$TRAVIS_TAG" ]; then
+   if [[ $MAC_EXEC == true ]]; then
+      sed -i '' -e "s!image: kabanero-operator:latest!image: kabanero-operator:$TRAVIS_TAG!g" $DEST
+   else
+      sed -i "s!image: kabanero-operator:latest!image: kabanero-operator:$TRAVIS_TAG!g" $DEST
+   fi
+fi
