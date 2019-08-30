8a2db5877d4a7ab45cb2031eb8ac1ec0c8533543
Kyle Schlosser <kyle.s.schlosser@gmail.com>
2019-08-30 08:21:32 -0500

    Add directive processor

--- pkg/controller/collection/archive.go
+++ pkg/controller/collection/archive.go
@@ -1,17 +1,17 @@
 package collection
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"archive/tar"
-	"compress/gzip"
-	"bytes"
 	"io"
+	"io/ioutil"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"net/http"
+	"strings"
 )
 
 func DownloadToByte(url string) ([]byte, error) {
@@ -24,23 +24,19 @@ func DownloadToByte(url string) ([]byte, error) {
 	return b, err
 }
 
-
 //Read the manifests from a tar.gz archive
 //It would be better to use the manifest.yaml as the index, and check the signatures
 //For now, ignore manifest.yaml and return all other yaml files from the archive
-func DecodeManifests (archive []byte) ([]unstructured.Unstructured, error) {
-
+func DecodeManifests(archive []byte, collectionName string) ([]unstructured.Unstructured, error) {
 	manifests := []unstructured.Unstructured{}
 
 	r := bytes.NewReader(archive)
 	gzReader, err := gzip.NewReader(r)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("Could not read manifest gzip"))
-		}
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Could not read manifest gzip"))
+	}
 	tarReader := tar.NewReader(gzReader)
-	
-	decoder := yaml.NewYAMLToJSONDecoder(tarReader)
-	
+
 	for {
 		header, err := tarReader.Next()
 
@@ -57,6 +53,18 @@ func DecodeManifests (archive []byte) ([]unstructured.Unstructured, error) {
 		case header.Name == "./manifest.yaml":
 			break
 		case strings.HasSuffix(header.Name, ".yaml"):
+			//Buffer the document for further processing
+			b := make([]byte, header.Size)
+			_, err := tarReader.Read(b)
+
+			//Apply the Kabanero yaml directive processor
+			s := &DirectiveProcessor{}
+			b, err = s.Render(b, map[string]interface{}{"CollectionName": collectionName})
+			if err != nil {
+				return nil, err
+			}
+
+			decoder := yaml.NewYAMLToJSONDecoder(bytes.NewReader(b))
 			out := unstructured.Unstructured{}
 			err = decoder.Decode(&out)
 			if err != nil {
@@ -68,14 +76,13 @@ func DecodeManifests (archive []byte) ([]unstructured.Unstructured, error) {
 	return manifests, nil
 }
 
-
-func GetManifests(url string) ([]unstructured.Unstructured, error) {
-
+func GetManifests(url string, collectionName string) ([]unstructured.Unstructured, error) {
 	b, err := DownloadToByte(url)
 	if err != nil {
 		return nil, err
 	}
-	manifests, err := DecodeManifests(b)
+
+	manifests, err := DecodeManifests(b, collectionName)
 	if err != nil {
 		return nil, err
 	}
---
+++ pkg/controller/collection/directive_processor.go
@@ -0,0 +1,73 @@
+package collection
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+)
+
+// The DirectiveProcessor processes text processing directives found in the yaml source
+type DirectiveProcessor struct {
+}
+
+func (g DirectiveProcessor) Render(b []byte, context map[string]interface{}) ([]byte, error) {
+	//Search for directives
+	directiveExpr := regexp.MustCompile(`\s?(#Kabanero!.*)$`)
+	directives := make([]string, 0)
+	reader := bufio.NewReader(bytes.NewReader(b))
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		if directiveExpr.Match(line) {
+			directive := directiveExpr.FindStringSubmatch(string(line))[1]
+			directives = append(directives, directive)
+		}
+	}
+
+	text := string(b)
+	for _, directive := range directives {
+		var err error
+		text, err = g.process_directive(directive, text, context)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return []byte(text), nil
+}
+
+//process_directive processes an individual directive like: #Kabanero! on activate substitute CollectionName for text '${collection-name}'
+func (g DirectiveProcessor) process_directive(directive string, text string, context map[string]interface{}) (string, error) {
+	textSubstitutionExpr := regexp.MustCompile(`#Kabanero!\son\sactivate\s(substitute\s(CollectionName)\s(for text)\s'(.+?)')`)
+	if textSubstitutionExpr.MatchString(directive) {
+		groups := textSubstitutionExpr.FindStringSubmatch(directive)
+
+		//group[0]: e.g. #Kabanero! on activate substitute CollectionName for text '${collection-name}'
+		//group[1]: e.g. substitute CollectionName for text '${collection-name}'
+		key := groups[2]               // The variable found in the context (CollectionName)
+		substitution_type := groups[3] //e.g. for text
+
+		if substitution_type == "for text" {
+			text_to_replace := groups[4] //e.g. '${collection-name}'
+
+			//Prune the directive from the text first
+			text = strings.Replace(text, directive, "", 1)
+			text = strings.TrimSpace(text)
+
+			text = strings.ReplaceAll(text, text_to_replace, context[key].(string))
+
+			return text, nil
+		} else {
+			return "", fmt.Errorf("Unknown substitution: %v", substitution_type)
+		}
+	} else {
+		return "", fmt.Errorf("Unknown directive: %v", directive)
+	}
+}
