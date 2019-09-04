0a2d599c1e42823cfe48586f95dba58e59f97f4c
Kyle Schlosser <kyle.s.schlosser@gmail.com>
2019-09-04 10:14:22 -0500

    Update the prepare release scripts

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 BASEPATH=$(dirname $(dirname $0))
-DEST=$BASEPATH/deploy/kabanero-operators.yaml
 
-# Find we are running on MAC.
-MAC_EXEC=false
-macos=Darwin
-if [ "$(uname)" == "Darwin" ]; then
-   MAC_EXEC=true
-fi
+# Split the IMAGE variable into repository and tag parts
+# e.g. kabanero/kabanero-operator:0.1.1 -> kabanero/kabanero-operator
+IFS=’:’ read -ra REPOSITORY <<< "$IMAGE"
 
-# Prepare operator deployment file
-cat << EOF >> $DEST
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: kabanero
----
-EOF
-
-cat $BASEPATH/deploy/dependencies.yaml >> $DEST; echo "---" >> $DEST
-cat $BASEPATH/deploy/operator.yaml >> $DEST; echo "---" >> $DEST
-cat $BASEPATH/deploy/role.yaml >> $DEST; echo "---" >> $DEST
-cat $BASEPATH/deploy/role_binding.yaml >> $DEST; echo "---" >> $DEST
-cat $BASEPATH/deploy/service_account.yaml >> $DEST; echo "---" >> $DEST
-
-# Update the operator deployment image entry if TRAVIS_TAG is set
-if [ ! -z "$TRAVIS_TAG" ]; then
-   if [[ $MAC_EXEC == true ]]; then
-      sed -i '' -e "s!image: kabanero-operator:latest!image: kabanero-operator:$TRAVIS_TAG!g" $DEST
-   else
-      sed -i "s!image: kabanero-operator:latest!image: kabanero-operator:$TRAVIS_TAG!g" $DEST
-   fi
-fi
+# Levearge the gen_operator_deployment.sh script to generate deploy/kabanero-operators.yaml
+$BASEPATH/contrib/gen_operator_deployment.sh $REPOSITORY:$TRAVIS_TAG
