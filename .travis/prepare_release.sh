32023de1c5a9067012f88252864081297f640d15
Kyle Schlosser <kyleschlosser@users.noreply.github.com>
2019-09-07 13:01:03 -0500

    Prepare for 0.1.2 release (#172)

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -7,3 +7,6 @@ IFS=’:’ read -ra REPOSITORY <<< "$IMAGE"
 
 # Levearge the gen_operator_deployment.sh script to generate deploy/kabanero-operators.yaml
 $BASEPATH/contrib/gen_operator_deployment.sh $REPOSITORY:$TRAVIS_TAG
+
+# Assure pull policies are set
+sed -i.bak -e 's!imagePullPolicy: .*$!imagePullPolicy: Always!' deploy/kabanero-operators.yaml
