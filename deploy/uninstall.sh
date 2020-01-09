8c8ced891f65eaa16aa9035f496ccd3edb663c76
kaczyns <kaczynsk@us.ibm.com>
2020-01-09 15:25:25 -0500

    Kabanero 0.5.0-rc.1

--- deploy/uninstall.sh
+++ deploy/uninstall.sh
@@ -7,7 +7,7 @@ set -x pipefail
 # following line.
 APPSODY_UNINSTALL=1
 
-RELEASE="${RELEASE:-0.4.0}"
+RELEASE="${RELEASE:-0.5.0}"
 KABANERO_SUBSCRIPTIONS_YAML="${KABANERO_SUBSCRIPTIONS_YAML:-https://github.com/kabanero-io/kabanero-operator/releases/download/$RELEASE/kabanero-subscriptions.yaml}"
 KABANERO_CUSTOMRESOURCES_YAML="${KABANERO_CUSTOMRESOURCES_YAML:-https://github.com/kabanero-io/kabanero-operator/releases/download/$RELEASE/kabanero-customresources.yaml}"
 SLEEP_LONG="${SLEEP_LONG:-15}"
