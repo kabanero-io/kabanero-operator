9912981a53fb443d36e1c1525ffee2570cb3d6cc
Daniel Cleyrat <dacleyra@us.ibm.com>
2020-01-24 21:32:30 -0500

    pipeline name

--- tests/14-nodejs-loopback.sh
+++ tests/14-nodejs-loopback.sh
@@ -7,7 +7,7 @@ APP="sample-nodejs-loopback" \
 DOCKER_IMAGE="image-registry.openshift-image-registry.svc:5000/kabanero/${APP}" \
 APP_REPO="https://github.com/kabanero-io/${APP}/" \
 PIPELINE_RUN="${APP}-build-deploy-pl-run-kabanero" \
-PIPELINE_REF="${COLLECTION}-build-push-deploy-pl" \
+PIPELINE_REF="${COLLECTION}-build-deploy-pl" \
 DOCKER_IMAGE_REF="${APP}-docker-image" \
 GITHUB_SOURCE_REF="${APP}-git-source" \
 $(dirname "$0")/pipelinerun.sh
