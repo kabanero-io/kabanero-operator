c6bfe4e509ef2863ce26dac49adf9383dc6424b9
Daniel Cleyrat <dacleyra@us.ibm.com>
2020-02-12 13:46:17 -0500

    int-test stacks

--- tests/13-nodejs-express.sh
+++ tests/13-nodejs-express.sh
@@ -2,12 +2,12 @@
 
 set -Eeuox pipefail
 
-COLLECTION="nodejs-express"
+STACK="nodejs-express"
 APP="sample-nodejs-express" \
 DOCKER_IMAGE="image-registry.openshift-image-registry.svc:5000/kabanero/${APP}" \
 APP_REPO="https://github.com/kabanero-io/${APP}/" \
 PIPELINE_RUN="${APP}-build-deploy-pl-run-kabanero" \
-PIPELINE_REF="${COLLECTION}-build-deploy-pl" \
+PIPELINE_REF="${STACK}-build-deploy-pl" \
 DOCKER_IMAGE_REF="${APP}-docker-image" \
 GITHUB_SOURCE_REF="${APP}-git-source" \
 $(dirname "$0")/pipelinerun.sh
