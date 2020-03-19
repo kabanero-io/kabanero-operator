e61833d2d75b26bfee2a6f51afdbbf5b8115a100
Vijai Kalathur <kvijai@gmail.com>
2020-03-19 16:23:42 -0400

    Update 12-nodejs.sh

--- tests/12-nodejs.sh
+++ tests/12-nodejs.sh
@@ -4,7 +4,7 @@ set -Eeuox pipefail
 
 STACK="nodejs"
 APP="sample-nodejs" \
-DOCKER_IMAGE="image-registry.openshift-image-registry.svc:5000/kabanero/${APP}" \
+DOCKER_IMAGE="image-registry.openshift-image-registry.svc:5000/kabanero" \
 APP_REPO="https://github.com/kabanero-io/${APP}/" \
 PIPELINE_RUN="${APP}-build-deploy-pl-run-kabanero" \
 PIPELINE_REF="${STACK}-build-deploy-pl" \
