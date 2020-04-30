bc0537dbd12ebf697df0a9c9c23858524c77ad6c
kaczyns <kaczynsk@us.ibm.com>
2020-04-30 13:21:49 -0400

    fix repo location in prepare_release.sh

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -26,7 +26,7 @@ if [ -n "$TRAVIS_TAG" ] ; then
     IFS=’:’ read -ra REPOSITORY <<< "$REGISTRY_IMAGE"
 
     # Set the tag for the kabanero CatalogSource
-    REGISTRY_IMAGE_REPO_DIGEST=$(docker image inspect kabanero-operator-registry:$TRAVIS_TAG --format="{{index .RepoDigests 0}}")
+    REGISTRY_IMAGE_REPO_DIGEST=$(docker image inspect kabanero/kabanero-operator-registry:$TRAVIS_TAG --format="{{index .RepoDigests 0}}")
     sed -i.bak -e 's,kabanero/kabanero-operator-registry:latest,'$REGISTRY_IMAGE_REPO_DIGEST',' deploy/kabanero-subscriptions.yaml
 
     # Set the tag for the install script
