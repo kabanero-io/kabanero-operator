7d4351c47dc411aaf5d9f4650c87eb282c0d0fab
Vijai Kalathur <kvijai@gmail.com>
2020-05-01 15:23:46 -0400

    Update uninstall.sh

--- deploy/uninstall.sh
+++ deploy/uninstall.sh
@@ -115,6 +115,8 @@ oc delete -f $KABANERO_CUSTOMRESOURCES_YAML --ignore-not-found --selector kabane
 # Delete service account to used by pipelines
 oc delete -f $KABANERO_CUSTOMRESOURCES_YAML --ignore-not-found --selector kabanero.io/install=24-pipeline-sa,kabanero.io/namespace!=true
 
+# Delete service account to used by events
+oc delete -f $KABANERO_CUSTOMRESOURCES_YAML --ignore-not-found --selector kabanero.io/install=26-events-sa,kabanero.io/namespace!=true
 
 # CRDs still exist
 if [ `oc get crds kabaneros.kabanero.io --no-headers --ignore-not-found | wc -l` -gt 0 ] ; then 
