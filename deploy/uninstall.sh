9d4d91a66494e10e69a72fcb4dc51ed6458456c7
Daniel Cleyrat <dacleyra@us.ibm.com>
2020-05-14 10:35:22 -0400

    tektoncd dashboard v0.6.1.2

--- deploy/uninstall.sh
+++ deploy/uninstall.sh
@@ -103,8 +103,8 @@ oc delete namespaces --selector=kabanero.io/component=kappnav --ignore-not-found
 oc delete --ignore-not-found -f $KABANERO_CUSTOMRESOURCES_YAML --selector kabanero.io/install=25-triggers-role
 
 # Tekton Dashboard
-curl -s -L https://github.com/tektoncd/dashboard/releases/download/v0.6.1.1/openshift-tekton-webhooks-extension-release.yaml | sed "s/openshift-pipelines/tekton-pipelines/" | oc delete --ignore-not-found -f -
-curl -s -L https://github.com/tektoncd/dashboard/releases/download/v0.6.1.1/openshift-tekton-dashboard-release.yaml | sed "s/openshift-pipelines/tekton-pipelines/" | oc delete --ignore-not-found -f -
+curl -s -L https://github.com/tektoncd/dashboard/releases/download/v0.6.1.2/openshift-tekton-webhooks-extension-release.yaml | sed "s/openshift-pipelines/tekton-pipelines/" | oc delete --ignore-not-found -f -
+curl -s -L https://github.com/tektoncd/dashboard/releases/download/v0.6.1.2/openshift-tekton-dashboard-release.yaml | sed "s/openshift-pipelines/tekton-pipelines/" | oc delete --ignore-not-found -f -
 
 # Delete CustomResources, do not delete namespaces , which can lead to finalizer problems.
 oc delete -f $KABANERO_CUSTOMRESOURCES_YAML --ignore-not-found --selector kabanero.io/install=23-cr-network-policy,kabanero.io/namespace!=true
